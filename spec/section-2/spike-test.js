9a4aad01cb71e06c0a072a553d3c552caa3a89ec
Giorgi Tsereteli <giorgi.webdev@gmail.com>
2024-01-13 20:04:19 -0500

    Add comments and details on test type

--- spec/section-2/spike-test.js
+++ spec/section-2/spike-test.js
@@ -1,15 +1,25 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+/**
+ * Spike Test Concept:
+ * 
+ * A spike test is a type of performance testing that involves rapidly increasing or 
+ * decreasing the number of virtual users to evaluate how the system handles sudden 
+ * spikes or drops in load. The provided code simulates a spike by quickly increasing 
+ * the number of virtual users to 2000 and then ramping down to 0, allowing you to 
+ * observe the system's behavior under varying levels of load.
+ */
+
 export const options = {
     stages: [
         {
-            duration: '2m',
-            target: 2000
+            duration: '2m', // First stage: 2 minutes
+            target: 2000    // Ramp up to 2000 virtual users
         },
         {
-            duration: '1m',
-            target: 0
+            duration: '1m', // Second stage: 1 minute
+            target: 0       // Ramp down to 0 virtual users
         }
     ]
 };
--- spec/section-2/stress-test.js
+++ spec/section-2/stress-test.js
@@ -1,19 +1,29 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+/**
+ * Concept of a Load Test:
+ * 
+ * A load test is a type of performance testing that involves 
+ * evaluating how a system behaves under expected and peak load conditions. 
+ * The goal is to assess the system's performance, scalability, and 
+ * reliability by simulating a realistic number of concurrent users 
+ * or transactions.
+ */
+
 export const options = {
     stages: [
         {
-            duration: '10s',
-            target: 1000
+            duration: '10s',  // First stage: 10 seconds
+            target: 1000      // Simulate 1000 virtual users
         },
         {
-            duration: '30m',
-            target: 1000
+            duration: '30m',  // Second stage: 30 minutes
+            target: 1000      // Maintain 1000 virtual users
         },
         {
-            duration: '10s',
-            target: 0
+            duration: '10s',  // Third stage: 10 seconds
+            target: 0         // Ramp down to 0 virtual users
         }
     ]
 };
